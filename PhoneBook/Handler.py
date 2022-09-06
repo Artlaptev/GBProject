b5c22ed8bf19efb06313763778cac5634a943c36
temkalap <89065927+temkalap@users.noreply.github.com>
2022-09-06 21:28:31 +0300

    Реализован импорт и экспорт в JSON, решить ,что возвращает импорт

--- PhoneBook/Handler.py
+++ PhoneBook/Handler.py
@@ -1,22 +1,45 @@
-from contact import  contact
-import contact_book
+from contact import contact
+from contact_book import contact_book
 import json
 
-
 from abc import ABC, abstractmethod
+
 
 class Handler(ABC):
     def importin(self):
         pass
+
     def export(self):
         pass
 
+
 class JsonHandler(Handler):
     def importin(self):
-        pass
-    def export(self, item):
-        book=contact(item)
-        print(type(book))
+        file = open('test.json')
+        data = json.load(file)
+        for item in data:
+            contact_book.add_contact(
+                self, item.name, item.patronymic, item.surname, item.number
+            )
+
+
+    def export(contact_list):
+        data = []
+        for item in contact_list:
+            data.append(JsonHandler.get_dict(item))
+        file = open('test.json', 'w')
+        json.dump(data, file)
+        file.close()
+
+    def get_dict(item):
+        item = item
+        dict = {'id': item.id,
+                'name': item.name,
+                'patronymic': item.patronymic,
+                'surname': item.surname,
+                'number': item.number
+                }
+        return dict
 
 
 class XMLHandler(Handler):
--- PhoneBook/main.py
+++ PhoneBook/main.py
@@ -1,11 +1,17 @@
-import json
+import Handler
 
 import contact
 import contact_book
 
 
-with open("test.json","r") as read_file:
-    developer = json.load(read_file)
+# with open("test.json","r") as read_file:
+#     developer = json.load(read_file)
 
-
-item= contact.contact(1, "artem","laptev","ram","8904494434")
+# lst=[]
+# item= contact.contact(1, "artem","laptev","ram","8904494434")
+# lst.append(item)
+# item=contact.contact(2,"gena","petrovich","ivanov","2309120748")
+# lst.append(item)
+# Handler.JsonHandler.export(lst)
+data=Handler.JsonHandler.importin(self=None)
+print(data)
