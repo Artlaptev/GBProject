3c86acc93c80ca1419845c09b3f7f4a0398b0087
temkalap <89065927+temkalap@users.noreply.github.com>
2022-09-07 23:00:51 +0300

    реализованы методы импорта и экспорта

--- PhoneBook/Handler.py
+++ PhoneBook/Handler.py
@@ -1,28 +1,23 @@
-# from contact import contact
-# from contact_book import contact_book
+from contact import contact
+from contact_book import contact_book
 import json
 import xml.etree.ElementTree as ET
 
 from abc import ABC, abstractmethod
 
 
-class Handler(ABC):
-    def importin(self):
-        pass
-
-    def export(self):
-        pass
+def create_contact_by_dictionary(book, item):
+    book.add_contact(item['name'], item['patronymic'], item['surname'], item['number'])
 
 
-class JsonHandler(Handler):
+class JsonHandler():
     def importin(self):
         file = open('test.json')
         data = json.load(file)
+        book = contact_book()
         for item in data:
-            contact_book.add_contact(
-                self, item.name, item.patronymic, item.surname, item.number
-            )
-
+            create_contact_by_dictionary(book, item)
+        return book
 
     def export(contact_list):
         data = []
@@ -43,24 +38,39 @@ class JsonHandler(Handler):
         return dict
 
 
-class XMLHandler(Handler):
+class XMLHandler():
     def importin(self):
-        tree=ET.parse('test.xml')
-        root=tree.getroot()
-        for i in root:
-            test=i.getchildren()
-            for j in test:
-                test1=j.text
-                test1=test1
+        book = contact_book()
+        tree = ET.parse('test.xml')
+        root = tree.getroot()
+        for contact_xml in root:
+            dict_atrs = {}
+            for atr in contact_xml:
+                dict_atrs[atr.tag] = atr.text
+            print(dict_atrs)
+            create_contact_by_dictionary(book, dict_atrs)
+        return book
+
+    def export(self, lst):
+        data = ET.Element('CONTACTLIST')
+        for item in lst:
+            contact = ET.SubElement(data, 'CONTACT')
+            self.create_subel(contact, 'id', item.id)
+            self.create_subel(contact, 'name', item.name)
+            self.create_subel(contact, 'patronymic', item.patronymic)
+            self.create_subel(contact, 'surname', item.surname)
+            self.create_subel(contact, 'number', item.number)
+        dataxml = ET.tostring(data).decode('utf-8')
+        file = open('test.xml', 'w')
+        file.write(dataxml)
+
+    def create_subel(self, element, name, value):
+        subEL = ET.SubElement(element, name)
+        subEL.text = str(value)
 
 
-    def export(self):
-        data=ET.Element('CONTACTLIST')
-
-        contact=ET.SubElement(data,'CONTACT')
-
-
-
-test=XMLHandler
-content=test.importin(test)
-print(content)
+handler=JsonHandler()
+book=handler.importin()
+handler1 = XMLHandler()
+handler1.export(book.get_sorted())
+handler1.importin()
