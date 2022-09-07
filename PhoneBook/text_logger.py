b584b18f504b23765f8539a4689d3f5e1350aa95
AlexBurkh <alex.burkh@yandex.ru>
2022-09-07 23:50:48 +0300

    Попытка привязаться к кодировке

--- PhoneBook/text_logger.py
+++ PhoneBook/text_logger.py
@@ -11,13 +11,13 @@ class text_logger:
             os.mkdir("data")
 
     def INFO(self, message):
-        with open(self._info_log_path, 'a+') as f:
+        with open(self._info_log_path, 'a+', encoding="utf-8") as f:
             f.write(f"[ * ] [{datetime.now()}] INFO: \"{message}\" \n")
 
     def WARNING(self, message):
-        with open(self._warning_log_path, 'a+') as f:
+        with open(self._warning_log_path, 'a+', encoding="utf-8") as f:
             f.write(f"[ ? ] [{datetime.now()}] WARNING: \"{message}\" \n")
 
     def ERROR(self, message):
-        with open(self._error_log_path, 'a+') as f:
+        with open(self._error_log_path, 'a+', encoding="utf-8") as f:
             f.write(f"[ ! ] [{datetime.now()}] ERROR: \"{message}\" \n")
