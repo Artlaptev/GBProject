efd47cbe2101dccf67d4ba2a9977f3377484d8bd
AlexBurkh <alex.burkh@yandex.ru>
2022-09-07 23:37:01 +0300

    Правка логгера для автоматического создания папки data

--- PhoneBook/text_logger.py
+++ PhoneBook/text_logger.py
@@ -1,9 +1,14 @@
 from datetime import datetime
+import os
 
 class text_logger:
     _info_log_path = "data/info_log.txt"
     _warning_log_path = "data/warning_log.txt"
     _error_log_path = "data/error_log.txt"
+
+    def __init__(self):
+        if not os.path.exists("data"):
+            os.mkdir("data")
 
     def INFO(self, message):
         with open(self._info_log_path, 'a+') as f:
